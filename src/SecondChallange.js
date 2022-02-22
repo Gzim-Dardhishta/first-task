752d76f09cf0183a569fd93914b61bf997a42367
Gzim-DArdhishta <gzimdardhishta0gmail.com>
2022-02-23 00:12:25 +0100

    Completed

--- src/SecondChallange.js
+++ src/SecondChallange.js
@@ -4,111 +4,21 @@ import { BsPersonCircle } from 'react-icons/bs';
 
 function SecondChallange() {
 
-  const [newCard, setNewCard] = useState([
-    { publishedDate: '3.3.0(14.05.2018)', types: 'New', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'File system changed from Dokan to CBFS Host Cloud Drive is now a network drive Various bug fixes and stability improvments Share premissions re-design and optimatizion',button: 'Download', id: 1},
-    { publishedDate: '3.3.0(14.05.2018)', types: 'New', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'File system changed from Dokan to CBFS Host Cloud Drive is now a network drive Various bug fixes and stability improvments Share premissions re-design and optimatizion',button: 'Download', id: 2},
-    { publishedDate: '3.3.0(14.05.2018)', types: 'New', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'File system changed from Dokan to CBFS Host Cloud Drive is now a network drive Various bug fixes and stability improvments Share premissions re-design and optimatizion',button: 'Download', id: 3}
+  const [cards, setCards] = useState([
+    { class: 'new', publishedDate: '3.3.0(14.05.2018)', types: 'New', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'File system changed from Dokan to CBFS Host Cloud Drive is now a network drive Various bug fixes and stability improvments Share premissions re-design and optimatizion',button: 'Download', id: 1},
+    { class: 'fix', publishedDate: '3.1.0(20.05.2015)', types: 'Fix', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'Introducing Host CLoud Drive - virtual drive funcionality New Share options and menegment New, more user frindly design Sync optimizations Vaarious performance improvments and bug fixes ',button: 'Download', id: 1},
+    { class: 'improvement', publishedDate: '3.1.0(20.05.2015)', types: 'Improvement', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'Added Settings for Auto Start Added Update Notification Speed Optimization Bug Fixes',button: 'Download', id: 1},
+    { class: 'new', publishedDate: '3.3.0(14.05.2018)', types: 'New', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'File system changed from Dokan to CBFS Host Cloud Drive is now a network drive Various bug fixes and stability improvments Share premissions re-design and optimatizion',button: 'Download', id: 2},
+    { class: 'fix', publishedDate: '3.1.0(20.05.2015)', types: 'Fix', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'Introducing Host CLoud Drive - virtual drive funcionality New Share options and menegment New, more user frindly design Sync optimizations Vaarious performance improvments and bug fixes ',button: 'Download', id: 2},
+    { class: 'improvement', publishedDate: '3.1.0(20.05.2015)', types: 'Improvement', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'Added Settings for Auto Start Added Update Notification Speed Optimization Bug Fixes',button: 'Download', id: 2},
+    { class: 'new', publishedDate: '3.3.0(14.05.2018)', types: 'New', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'File system changed from Dokan to CBFS Host Cloud Drive is now a network drive Various bug fixes and stability improvments Share premissions re-design and optimatizion',button: 'Download', id: 3},
+    { class: 'fix', publishedDate: '3.1.0(20.05.2015)', types: 'Fix', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'Introducing Host CLoud Drive - virtual drive funcionality New Share options and menegment New, more user frindly design Sync optimizations Vaarious performance improvments and bug fixes ',button: 'Download', id: 3},
+    { class: 'improvement', publishedDate: '3.1.0(20.05.2015)', types: 'Improvement', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'Added Settings for Auto Start Added Update Notification Speed Optimization Bug Fixes',button: 'Download', id: 3}
   ]);
-
-  const[fixCard, setFixCard] = useState([
-    { publishedDate: '3.1.0(20.05.2015)', types: 'Fix', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'Introducing Host CLoud Drive - virtual drive funcionality New Share options and menegment New, more user frindly design Sync optimizations Vaarious performance improvments and bug fixes ',button: 'Download', id: 1},
-    { publishedDate: '3.1.0(20.05.2015)', types: 'Fix', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'Introducing Host CLoud Drive - virtual drive funcionality New Share options and menegment New, more user frindly design Sync optimizations Vaarious performance improvments and bug fixes ',button: 'Download', id: 2},
-    { publishedDate: '3.1.0(20.05.2015)', types: 'Fix', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'Introducing Host CLoud Drive - virtual drive funcionality New Share options and menegment New, more user frindly design Sync optimizations Vaarious performance improvments and bug fixes ',button: 'Download', id: 3}
-  ]);
-
-  const[improvmentCard, setImprovmentCard] = useState([
-    { publishedDate: '3.1.0(20.05.2015)', types: 'Improvement', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'Added Settings for Auto Start Added Update Notification Speed Optimization Bug Fixes',button: 'Download', id: 1},
-    { publishedDate: '3.1.0(20.05.2015)', types: 'Improvement', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'Added Settings for Auto Start Added Update Notification Speed Optimization Bug Fixes',button: 'Download', id: 2},
-    { publishedDate: '3.1.0(20.05.2015)', types: 'Improvement', photo: <BsPersonCircle/>, name: 'Kevin Joe', info: 'Added Settings for Auto Start Added Update Notification Speed Optimization Bug Fixes',button: 'Download', id: 3}
-  ]);
-
-
-  const cardStyle = {
-
-    title: {
-      width: '50%',
-      marginLeft: 'auto',
-      marginTop: '100px'
-    },
-
-    cardPosition: {
-      display: "flex",
-      justifyContent: 'space-around',
-      width: '80%',
-    },
-
-    cardBorder: {
-      border: '1px solid lightgray',
-      borderRadius: '25px',
-      marginBottom: '40px',
-      height: '400px',
-      width: '380px',
-    },
-
-    cardInfo: {
-      margin: '30px',
-    },
-
-    publishDate: {
-      fontWeight: '400',
-      marginBottom: '30px'
-    },
-
-    new: {
-      padding: '5px 30px 5px 30px',
-      backgroundColor: 'rgb(1, 200, 83)',
-      borderRadius: '5px',
-      marginRight: '25px'
-    },
-
-    fix: {
-      padding: '5px 30px 5px 30px',
-      backgroundColor: 'rgb(41, 98, 255)',
-      borderRadius: '5px',
-      marginRight: '25px'
-    },
-
-    improvment: {
-      color: 'white',
-      padding: '5px 30px 5px 30px',
-      backgroundColor: 'rgb(195, 91, 255)',
-      borderRadius: '5px',
-      marginRight: '25px'
-    },
-
-    photo: {
-      fontSize: '25px',
-      position: 'relative',
-      top: '5px',
-      marginRight: '25px'
-    },
-
-    name: {
-      fontWeight: '400',
-      fontSize: '18px'
-    },
-
-    info: {
-      color: 'lightgray',
-      fontSize: '17px',
-      marginTop: '30px',
-      height: '80px'
-    },
-
-    download: {
-      position: 'relative',
-      top: '60px',
-      color: 'rgb(72, 121,255)',
-      padding: '20px 50px',
-      border: '1px solid rgb(72, 121,255)',
-      borderRadius: '30px',
-      backgroundColor: 'white',
-    }
-  }
 
   return (
     <div>
-        <UpdatesCard newCard = { newCard } fixCard = { fixCard } improvmentCard = { improvmentCard } cardStyle = {cardStyle}  title = "SecondChallange"/>
+        <UpdatesCard cards = {cards}  title = "SecondChallange"/>
     </div>
   )
 }
--- src/UpdatesCard.js
+++ src/UpdatesCard.js
@@ -1,56 +1,23 @@
 import React from 'react';
 
-function UpdatesCard( {newCard, fixCard, improvmentCard, cardStyle, title}  ) {
+function UpdatesCard( { cards, title}  ) {
 
   return (
-
     <section>
-      <h2 style={cardStyle.title}> { title }</h2>
-      <div style={cardStyle.cardPosition}>
-        <div>
-          {newCard.map((card) => (
-            <div style = {cardStyle.cardBorder} key={card.id}>
-              <div style={cardStyle.cardInfo}>
-                <h2 style={ cardStyle.publishDate }>{ card.publishedDate }</h2>
-                <span style={cardStyle.new}>{ card.types }</span>
-                <span style={cardStyle.photo}>{ card.photo }</span>
-                <span style={cardStyle.name}>{ card.name }</span>
-                <p style={cardStyle.info}>{ card.info }</p>
-                <button style={cardStyle.download}>{ card.button }</button>
+      <h2 className='title'> { title }</h2>
+      <div className='card-position'>
+          {cards.map((card) => (
+            <div className='card-border' key={card.id}>
+              <div className='card-info'>
+                <h2 className='publish-date'>{ card.publishedDate }</h2>
+                <span className={`types ${card.class}`}>{ card.types }</span>
+                <span className='photo'>{ card.photo }</span>
+                <span className='name'>{ card.name }</span>
+                <p className='info'>{ card.info }</p>
+                <button className='download'>{ card.button }</button>
               </div>
             </div>  
           ))}
-        </div>
-
-        <div>
-          {fixCard.map((card) => (
-            <div style = {cardStyle.cardBorder} key={card.id}>
-              <div style={cardStyle.cardInfo}>
-                <h2 style={ cardStyle.publishDate }>{ card.publishedDate }</h2>
-                <span style={cardStyle.fix}>{ card.types }</span>
-                <span style={cardStyle.photo}>{ card.photo }</span>
-                <span style={cardStyle.name}>{ card.name }</span>
-                <p style={cardStyle.info}>{ card.info }</p>
-                <button style={cardStyle.download}>{ card.button }</button>
-              </div>
-            </div>  
-          ))}
-        </div>
-
-        <div>
-          {improvmentCard.map((card) => (
-            <div style = {cardStyle.cardBorder } key={card.id}>
-              <div style={cardStyle.cardInfo}>
-                <h2 style={ cardStyle.publishDate }>{ card.publishedDate }</h2>
-                <span style={cardStyle.improvment}>{ card.types }</span>
-                <span style={cardStyle.photo}>{ card.photo }</span>
-                <span style={cardStyle.name}>{ card.name }</span>
-                <p style={cardStyle.info}>{ card.info }</p>
-                <button style={cardStyle.download} >{ card.button }</button>
-              </div>
-            </div>  
-          ))}
-        </div>
       </div>
     </section>
   )
